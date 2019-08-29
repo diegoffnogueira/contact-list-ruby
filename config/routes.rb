08d9aa46452fbabe5b83d7ea004386a9cf96e5cc
Diego Nogueira <diegof.fnogueira@gmail.com>
2019-08-29 01:46:49 -0300

    Contact list ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+
   resources :contacts
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "static_pages#index"
+  root to: 'static_pages#index'
 
-  get "sobre", to: "static_pages#sobre"
-  get "contato", to: "static_pages#contato"
+  get 'sobre', to: 'static_pages#sobre'
+  get 'contato', to: 'static_pages#contato'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sesssions#destroy'
 
 end
